9f99f97632c62c709b660943dbd2acce5c34ad2f
Taavi Rehemägi <taavi@toggl.com>
2015-04-17 19:45:20 +0300

    match phrase from text

--- static/js/script.js
+++ static/js/script.js
@@ -1,3 +1,24 @@
-$("#textareaID").on('input', function (){
-    console.log("sada");
+$( document ).ready(function() {
+
+	$("#textareaID").on('input', function (){
+		searchKeyword();
+	});
+
+	$.getJSON("js/keyword_lib.json", function(data){
+		jsonList = data;
+	});
+
+	function searchKeyword(){
+		textareaValue = $("#textareaID").val();	
+		$.each(jsonList, function(key, value){
+			if(textareaValue.indexOf(value.key) >= 0){
+				activateGif(value.gif)
+			}
+		});
+	}
+
+	function activateGif(gif){
+		console.log("activating" + gif);
+	}
 });
+
