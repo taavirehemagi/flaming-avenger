22599d334e0cf33d550e751d2f9ec3f7e43702e8
Taavi Rehemägi <taavi@toggl.com>
2015-04-20 11:44:07 +0300

    layoutfix.

--- static/js/script.js
+++ static/js/script.js
@@ -24,15 +24,16 @@ $( document ).ready(function() {
 	function activateGif(gif, duration){
 		if(theaterMode){
 			$('#easter-egg').css("background-image", "url(gif/"+gif+")");
+			$('#easter-egg').css("display", "block");
 		}else{
 			$('#support-img').css("background-image", "url(gif/"+gif+")");
 		}
 		setTimeout(restoreImage, duration);
 	}
 	
-
 	function restoreImage(){
 		if(theaterMode){
+			$('#easter-egg').css("display", "none");
 			$('#easter-egg').css("background-image", "");
 		}else{
 			$('#support-img').css("background-image", "url(https://toggl.com/app/images/help/support.5fef2ac9.png)");
